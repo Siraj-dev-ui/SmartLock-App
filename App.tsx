2b3a31b3d2f5e89b004501873a9e9cf0e288f61f
Siraj-dev-ui <sirajalig86@gmail.com>
2025-06-19 16:29:14 +0200

    Context added

--- App.tsx
+++ App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import MainNavigation from './src/Navigations/MainNavigation';
 import {PaperProvider} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {ToastProvider} from 'react-native-toast-notifications';
+import AppProviders from './src/Contexts/AppProvider';
 
 function App(): React.JSX.Element {
   return (
-    <PaperProvider>
-      <MainNavigation />
-    </PaperProvider>
+    <AppProviders>
+      <PaperProvider>
+        <ToastProvider>
+          <MainNavigation />
+        </ToastProvider>
+      </PaperProvider>
+    </AppProviders>
   );
 }
 
