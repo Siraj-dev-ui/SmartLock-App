93425e588ab6da84c7e13020dcd14d58d7536aea
Siraj-dev-ui <sirajalig86@gmail.com>
2025-07-02 21:52:23 +0200

    Urls

--- src/Utils/Urls.js
+++ src/Utils/Urls.js
@@ -1,3 +1,3 @@
-// export const BASE_URL_LOCAL = 'http://192.168.0.104:3000';
-export const BASE_URL_LOCAL = 'http://192.168.86.67:3000';
+export const BASE_URL_LOCAL = 'http://192.168.0.104:3000';
+// export const BASE_URL_LOCAL = 'http://192.168.254.67:3000';
 export const BASE_URL_LIVE = 'https://tricky-tan-beanie.cyclic.app';
