77670629cc06ecad399fb077c4b9931fb02398db
Siraj-dev-ui <sirajalig86@gmail.com>
2025-07-02 23:36:23 +0200

    url live

--- src/Utils/Urls.js
+++ src/Utils/Urls.js
@@ -1,3 +1,3 @@
 export const BASE_URL_LOCAL = 'http://192.168.0.104:3000';
 // export const BASE_URL_LOCAL = 'http://192.168.254.67:3000';
-export const BASE_URL_LIVE = 'https://tricky-tan-beanie.cyclic.app';
+export const BASE_URL_LIVE = 'https://smartlock-nestapi.onrender.com';
