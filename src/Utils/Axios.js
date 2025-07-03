b55798db5dc383d9cf6142799312d12a1510851c
Siraj-dev-ui <sirajalig86@gmail.com>
2025-07-03 16:50:45 +0200

    interceptor disabled

--- src/Utils/Axios.js
+++ src/Utils/Axios.js
@@ -7,31 +7,31 @@ export const axios = axiosIns.create({
 });
 
 // ✅ Add request interceptor
-axios.interceptors.request.use(
-  request => {
-    console.log(
-      `[API Request] ${request.method?.toUpperCase()} ${request.baseURL}${
-        request.url
-      }`,
-    );
-    console.log('Payload:', request.data || request.params);
-    return request;
-  },
-  error => {
-    console.error('[Request Error]', error);
-    return Promise.reject(error);
-  },
-);
+// axios.interceptors.request.use(
+//   request => {
+//     console.log(
+//       `[API Request] ${request.method?.toUpperCase()} ${request.baseURL}${
+//         request.url
+//       }`,
+//     );
+//     console.log('Payload:', request.data || request.params);
+//     return request;
+//   },
+//   error => {
+//     console.error('[Request Error]', error);
+//     return Promise.reject(error);
+//   },
+// );
 
 // ✅ Add response interceptor (optional)
-axios.interceptors.response.use(
-  response => {
-    console.log(`[API Response] ${response.status} ${response.config.url}`);
-    console.log('Response Data: ', response.data);
-    return response;
-  },
-  error => {
-    console.error('[Response Error]', error.response?.data || error.message);
-    return Promise.reject(error);
-  },
-);
+// axios.interceptors.response.use(
+//   response => {
+//     console.log(`[API Response] ${response.status} ${response.config.url}`);
+//     console.log('Response Data: ', response.data);
+//     return response;
+//   },
+//   error => {
+//     console.error('[Response Error]', error.response?.data || error.message);
+//     return Promise.reject(error);
+//   },
+// );
