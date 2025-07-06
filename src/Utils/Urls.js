154ef1a6731e2c09d3c3b9dc387cb7e4628934f3
Siraj-dev-ui <sirajalig86@gmail.com>
2025-07-06 13:11:32 +0200

    Urls File added for Evaluation

---
+++ src/Utils/Urls.js
@@ -0,0 +1,5 @@
+// export const BASE_URL_LOCAL = 'http://192.168.0.104:3000';
+export const BASE_URL_LOCAL = 'http://192.168.254.67:3000';
+// export const BASE_URL_LOCAL = 'http://192.168.254.215:3000';
+export const BASE_URL_LIVE = 'https://smartlock-nestapi.onrender.com';
+// export const PROD_BASE_URL = 'https://smartlock-nestapi.onrender.com';
